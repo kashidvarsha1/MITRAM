698be16a6b11a5fe4ebccfdc5d29e3df6579be55
riyabhosale2530-ops <riyabhosale2530@gmail.com>
2025-11-03 12:21:06 +0530

    MITRAM frontpage

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple button click functions
+document.getElementById("signupBtn").addEventListener("click", function() {
+  alert("Signup button clicked! (Redirect to signup page)");
+});
+
+document.getElementById("loginBtn").addEventListener("click", function() {
+  alert("Login button clicked! (Redirect to login page)");
+});
